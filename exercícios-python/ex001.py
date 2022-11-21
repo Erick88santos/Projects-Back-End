5b8da3512d67c2dc2ae584d323f4b1aa41d4ba53
Erick Santos <71516100+Erick88santos@users.noreply.github.com>
2022-11-21 20:27:56 -0300

    média de nota

---
+++ exercícios-python/ex001.py
@@ -0,0 +1,16 @@
+# Média de notas Escolares em 4 períodos.
+## **Python**  🐍 
+
+
+notas = []
+contador = 1
+
+while contador < 5:
+    notas.append(float(input(f'Informe a {contador}a nota: ')))
+    contador += 1
+
+maior_nota = max(notas)
+menor_nota = min(notas)
+media = sum(notas) / len(notas)
+
+print(f'A média final é {media:.2f}. A sua maior nota foi {maior_nota:.2f} e a menor foi {menor_nota:.2f}')
